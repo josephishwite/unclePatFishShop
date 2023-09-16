b45a164cf230e620bf9451ea2629c6d89a4732fd
josephiswhite2 <joseph1998@hotmail.sg>
2023-09-16 14:45:23 +0800

    Navbar and added bootstrap

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { CartDialogComponent } from './cart-dialog/cart-dialog.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+
+  constructor(public dialog: MatDialog){}
+
+  openCartDialog(): void{
+    const dialogRef = this.dialog.open(CartDialogComponent, {
+      width: '400px', // Set the desired width
+      // Other options like data, height, etc.
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      // Handle modal close actions if needed
+    });
+  }
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,9 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowseComponent } from './browse/browse.component';
 import { AdminHomeComponent } from './admin-home/admin-home.component';
+import { BrowseComponent } from './browse/browse.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatDialogModule } from "@angular/material/dialog";
 import { CartDialogComponent } from './cart-dialog/cart-dialog.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -16,7 +19,11 @@ import { CartDialogComponent } from './cart-dialog/cart-dialog.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatDialogModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
